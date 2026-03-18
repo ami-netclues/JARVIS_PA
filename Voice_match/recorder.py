dfb7c19c569a94116e70c0df43cbf4fe6629e714
Ami <Ami.Bhavsar@netclues.local>
2026-03-18 16:56:46 +0530

    empty branch

--- Voice_match/recorder.py
+++
@@ -1,9 +0,0 @@
-import sounddevice as sd
-from scipy.io.wavfile import write
-
-def record_audio(filename, duration=5, fs=16000):
-    print("Recording...")
-    audio = sd.rec(int(duration * fs), samplerate=fs, channels=1)
-    sd.wait()
-    write(filename, fs, audio)
-    print("Saved:", filename)
